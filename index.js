6f2bf264665bc4aef270bfd6162525a0d22489dc
sedrftyghu <gs1ajdhcndj@hotmail.com>
2024-05-06 09:15:24 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-function largestDivisibleSubset(nums) {
-  nums.sort((a, b) => a - b);
-  const dp = new Array(nums.length).fill(1);
-  let maxSubsetSize = 1;
-  let maxSubsetIdx = 0;
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] % nums[j] === 0) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-        if (dp[i] > maxSubsetSize) {
-          maxSubsetSize = dp[i];
-          maxSubsetIdx = i;
-        }
-      }
-    }
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) left = mid + 1;
+    else if (nums[mid] < nums[right]) right = mid;
+    else right--;
   }
-  const result = [];
-  let prev = nums[maxSubsetIdx];
-  let count = maxSubsetSize;
-  for (let i = maxSubsetIdx; i >= 0; i--) {
-    if (prev % nums[i] === 0 && dp[i] === count) {
-      result.unshift(nums[i]);
-      prev = nums[i];
-      count--;
-    }
-  }
-  return result;
+  return nums[left];
 }
