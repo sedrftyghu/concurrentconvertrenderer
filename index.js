0c81385db9057302a6fb94dd879c9a60dda63d61
sedrftyghu <gs1ajdhcndj@hotmail.com>
2024-05-07 14:00:13 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function findMin(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) left = mid + 1;
-    else if (nums[mid] < nums[right]) right = mid;
-    else right--;
+function permute(nums) {
+  const result = [];
+  backtrack([]);
+  return result;
+  function backtrack(current) {
+    if (current.length === nums.length) {
+      result.push([...current]);
+      return;
+    }
+    for (const num of nums) {
+      if (current.includes(num)) continue;
+      current.push(num);
+      backtrack(current);
+      current.pop();
+    }
   }
-  return nums[left];
 }
